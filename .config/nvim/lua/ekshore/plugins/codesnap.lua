ddaf157186fd21bbcab71aedc3797bead8f3c81a
Caleb Ekstrand <ekstrand.caleb@ekshore.net>
2024-05-22 21:18:19 -0500

    Codesnap updates

--- .config/nvim/lua/ekshore/plugins/codesnap.lua
+++ .config/nvim/lua/ekshore/plugins/codesnap.lua
@@ -1,15 +1,19 @@
 return {
-    "mistricky/codesnap.nvim",
-    lazy = "true",
-    build = "make",
-    cmd = "CodeSnapPreviewOn",
+    'mistricky/codesnap.nvim',
+    lazy = 'true',
+    build = 'make',
+    event = 'VeryLazy',
     opts = {
         mac_window_bar = true,
-        watermark = "",
-        preview_title = "",
-        editor_font_family = "Hack Nerd Font Mono",
+        watermark = '',
+        preview_title = '',
+        editor_font_family = 'Hack Nerd Font Mono',
+        has_breadcrumbs = true,
+        has_line_number = true,
     },
     config = function(_, opts)
-        require("codesnap").setup(opts)
-    end,
+        require('codesnap').setup(opts)
+        vim.keymap.set('v', '<leader>cc', ':CodeSnap<CR>')
+        vim.keymap.set('v', '<leader>cs', ':CodeSnapSave<CR>')
+    end
 }
